e48523629580e43cc6c7f7705b7faf34ce113583
Alex <alex.moros.marco@gmail.com>
2016-05-11 04:41:50 +0200

    problem of exercise 3 written

---
+++ exercises/3-js/problem/js/index.js
@@ -0,0 +1,22 @@
+// Start in strict mode so that non-defined variables give an error.
+"use strict";
+
+/*****************
+ ** APP STARTUP **
+ *****************/
+(function() {
+  function onLoaded() {
+    /**
+     * TODO: Exercise 3.3: Print to the console the version of the app.
+     */
+    // Register events on elements now that the DOM content is loaded
+    /**
+     * TODO: Exercise 3.4: Register the click event of the menu button
+     * and show an alert informing the version of the app.
+     */
+  }
+
+  document.addEventListener('DOMContentLoaded', function(event) {
+    onLoaded();
+  }, false);
+}());
