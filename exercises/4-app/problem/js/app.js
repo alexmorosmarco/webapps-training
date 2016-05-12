f2b2270fdca5a99d9f6b6c643763a5c61342d652
Alex <alex.moros.marco@gmail.com>
2016-05-12 02:55:20 +0200

    exercise 4 problem

---
+++ exercises/4-app/problem/js/app.js
@@ -0,0 +1,48 @@
+// Start in strict mode so that non-defined variables give an error.
+"use strict";
+
+var App = {
+  version: '0.4.0',
+  productsApi: 'http://demo8844552.mockable.io/products',
+  /**
+   * TODO: Exercise 4.1: create a new property to store the number of products.
+   */
+
+  /**
+   * TODO: Exercise 4.2: implement below function so that it creates the DOM
+   * article element for the given product JS object. Returns the element.
+   *
+   * Input:
+   * product: JS object representing a product. Example:
+   * {
+   *    title: "The perfect menu",
+   *    description: "Toasts with garlic, tomato and ham",
+   *    imageUrl: "http://lorempixel.com/300/200/food/1/",
+   *    price: "6"
+   * }
+   *
+   * Output markup of the article:
+   * <article>
+   *   <img src="http://lorempixel.com/300/200/food/1" alt="product">
+   *   <div>The perfect menu</div>
+   *   <div>Toasts with garlic, tomato and ham</div>
+   *   <div class="horizontal-layout">
+   *     <div>6€</div>
+   *     <div>Add</div>
+   *   </div>
+   * </article>
+   */
+  createProductArticle: function (product) {
+    // Implementation here
+  },
+  /**
+   * TODO: Exercise 4.4: implement below function so that it updates the Cart
+   * info with the number of products stored in App.productsNumber: "Cart (1)".
+   *
+   * Then call this function when product Add button is clicked, but just before
+   * that update App.productsNumber increasing it by 1 unit.
+   */
+  reloadCartInfo: function () {
+    // Implementation here
+  }
+};
---
+++ exercises/4-app/problem/js/index.js
@@ -0,0 +1,36 @@
+// Start in strict mode so that non-defined variables give an error.
+"use strict";
+
+/*****************
+ ** APP STARTUP **
+ *****************/
+(function() {
+  function onLoaded() {
+    console.log('App v' + App.version);
+    // Register events on elements now that the DOM content is loaded
+    var menuButton = document.getElementById('menu-button');
+    menuButton.addEventListener('click', function(event) {
+      alert('Info: App v' + App.version);
+    }, false);
+
+    /**
+     * TODO: Exercise 4.5: show the number of products in the cart info, if
+     * the number is stored in the localStorage, so that the user does not
+     * lose the session if he reloads the web app.
+     */
+
+
+    // Get products from server and add them to the view
+    /**
+     * TODO: Exercise 4.3: get the products from the server URL
+     * stored in App.productsApi using a XHR request. Then
+     * create DOM articles through App.createProductArticle function on "app.js"
+     * and insert them in the main container.
+     */
+
+  }
+
+  document.addEventListener('DOMContentLoaded', function(event) {
+    onLoaded();
+  }, false);
+}());
